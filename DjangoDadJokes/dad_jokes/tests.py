83bdf2bfa96682bcd01e06b32ef6ae90793dad03
aronf <aronfreyrh@gmail.com>
2025-06-30 16:25:47 +0200

    Adding a missing urls.py and setting up a basic test.

--- DjangoDadJokes/dad_jokes/tests.py
+++ DjangoDadJokes/dad_jokes/tests.py
@@ -1,3 +1,24 @@
+import requests
 from django.test import TestCase
 
-# Create your tests here.
+# Get the EXTERNAL_API_URL from the settings
+from django.conf import settings
+
+class DadJokesFromAPI(TestCase):
+    """
+    Test case for the Dad Jokes API.
+    """
+
+    def setUp(self):
+        self.api_url: str = settings.EXTERNAL_API_URL
+
+    def test_get_joke_from_api(self):
+        """
+        Test that we can retrieve a joke from the API.
+        """
+        headers = {'Accept': 'application/json'}
+        resp = requests.get(self.api_url, headers=headers, timeout=1)
+        self.assertEqual(resp.status_code, 200)
+        data = resp.json()
+        self.assertIn('joke', data)
+        self.assertIn('id', data)
