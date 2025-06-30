dd956a6eaf2de4006cab8339f4158fbdf978056c
aronf <aronfreyrh@gmail.com>
2025-06-30 18:05:12 +0200

    Getting tests running for the CRUD operations.

--- DjangoDadJokes/dad_jokes/tests.py
+++ DjangoDadJokes/dad_jokes/tests.py
@@ -1,9 +1,12 @@
 import requests
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
 
 # Get the EXTERNAL_API_URL from the settings
 from django.conf import settings
 from .utils import get_dad_joke_from_api, DadJoke as DadJokeObj
+from .models import DadJoke
 
 class DadJokesFromAPI(TestCase):
     """
@@ -30,3 +33,41 @@ class DadJokesFromAPI(TestCase):
         self.assertIsInstance(dad_joke.joke, str)
         self.assertIsInstance(dad_joke.site_id, str)
         self.assertGreater(len(dad_joke.joke), 0)
+
+class DadJokeCrudOperations(APITestCase):
+    """
+    Test case for CRUD operations on Dad Jokes.
+    """
+
+    def setUp(self):
+        self.dad_joke = DadJokeObj(joke="This is a test joke", site_id="test123")
+        self.dad_joke_url = '/dad-jokes/dad-jokes/'
+        # save the joke to the database
+        self.test_joke = DadJoke.objects.create(joke=self.dad_joke.joke, site_id=self.dad_joke.site_id)
+
+
+    def test_list_jokes(self):
+        response = self.client.get(self.dad_joke_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_joke(self):
+        data = {"joke": "Another test joke", "site_id": "test456"}
+        response = self.client.post(self.dad_joke_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['joke'], data['joke'])
+
+    def test_retrieve_joke(self):
+        response = self.client.get(f'{self.dad_joke_url}{self.test_joke.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['joke'], self.dad_joke.joke)
+
+    def test_update_joke(self):
+        data = {"joke": "Updated joke", "site_id": self.dad_joke.site_id}
+        response = self.client.put(f'{self.dad_joke_url}{self.test_joke.id}/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['joke'], data['joke'])
+
+    def test_delete_joke(self):
+        response = self.client.delete(f'{self.dad_joke_url}{self.test_joke.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(DadJoke.objects.filter(id=self.test_joke.id).exists())
