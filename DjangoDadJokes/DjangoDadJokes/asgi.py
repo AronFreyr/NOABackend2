8b7d10b9334a5985477df929234472c4f072643e
aronf <aronfreyrh@gmail.com>
2025-06-30 15:50:14 +0200

    Fixing an unfortunate naming oversight by changing the project name from "DjangodadJokes" to "DjangoDadJokes".

--- DjangoDadJokes/DjangoDadJokes/asgi.py
+++ DjangoDadJokes/DjangoDadJokes/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for DjangodadJokes project.
+ASGI config for DjangoDadJokes project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangodadJokes.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoDadJokes.settings')
 
 application = get_asgi_application()
--- DjangoDadJokes/DjangoDadJokes/wsgi.py
+++ DjangoDadJokes/DjangoDadJokes/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DjangodadJokes project.
+WSGI config for DjangoDadJokes project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangodadJokes.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoDadJokes.settings')
 
 application = get_wsgi_application()
